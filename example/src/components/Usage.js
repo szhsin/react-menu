3887bfd5a3af3533eb72cefb0980932b85470ef8
Zheng Song <41896553+szhsin@users.noreply.github.com>
2020-09-18 01:45:27 +1000

    Extract examples into separate function components

--- example/src/components/Usage.js
+++ example/src/components/Usage.js
@@ -9,16 +9,11 @@ import {
     MenuDivider
 } from '@szhsin/react-menu';
 import { TableContents } from './TableContents';
-import { codeExamples } from '../data/codeExamples';
 import { Example } from './Example';
+import { codeExamples } from '../data/codeExamples';
 
 
 export const Usage = React.memo(function Usage() {
-
-    const [textColor, setTextColor] = useState('red');
-    const [isBold, setBold] = useState(true);
-    const [isItalic, setItalic] = useState(false);
-    const [isUnderline, setUnderline] = useState(false);
 
     const tableContents = [
         {
@@ -47,125 +42,186 @@ export const Usage = React.memo(function Usage() {
 
             <main id="usage">
                 <h1>Usage</h1>
-                <Example initialFullSource={true} data={codeExamples.basicMenu} >
-                    <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
-                        <MenuItem>New File</MenuItem>
-                        <MenuItem>Save</MenuItem>
-                        <MenuItem>Close Window</MenuItem>
-                    </Menu>
-                </Example>
-
-                <Example data={codeExamples.subMenu} >
-                    <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
-                        <MenuItem>New File</MenuItem>
-                        <SubMenu label={'Open Recent'}>
-                            <MenuItem>index.html</MenuItem>
-                            <MenuItem>example.js</MenuItem>
-                            <MenuItem>about.css</MenuItem>
-                        </SubMenu>
-                        <MenuItem>Save</MenuItem>
-                    </Menu>
-                </Example>
-
-                <Example data={codeExamples.radioGroup} >
-                    <Menu menuButton={<MenuButton>Text color</MenuButton>}>
-                        <MenuRadioGroup
-                            value={textColor}
-                            onChange={e => setTextColor(e.value)}>
-                            <MenuItem value={'red'}>Red</MenuItem>
-                            <MenuItem value={'green'}>Green</MenuItem>
-                            <MenuItem value={'blue'}>Blue</MenuItem>
-                        </MenuRadioGroup>
-                    </Menu>
-
-                    <div className="sample-text" style={{ color: textColor }}>
-                        Sample text
-                </div>
-                </Example>
-
-                <Example data={codeExamples.checkBox} >
-                    <Menu menuButton={<MenuButton>Text style</MenuButton>}>
-                        <MenuItem type="checkbox" checked={isBold}
-                            onClick={e => setBold(e.checked)}>
-                            Bold
-                    </MenuItem>
-                        <MenuItem type="checkbox" checked={isItalic}
-                            onClick={e => setItalic(e.checked)}>
-                            Italic
-                    </MenuItem>
-                        <MenuItem type="checkbox" checked={isUnderline}
-                            onClick={e => setUnderline(e.checked)}>
-                            Underline
-                    </MenuItem>
-                    </Menu>
-
-                    <div className="sample-text" style={{
-                        fontWeight: isBold ? 'bold' : 'initial',
-                        fontStyle: isItalic ? 'italic' : 'initial',
-                        textDecoration: isUnderline ? 'underline' : 'initial'
-                    }}>Sample text</div>
-                </Example>
-
-                <Example data={codeExamples.headerAndDivider} >
-                    <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
-                        <MenuItem>New File</MenuItem>
-                        <MenuItem>Save</MenuItem>
-                        <MenuItem>Close Window</MenuItem>
-                        <MenuDivider />
-                        <MenuHeader>Edit</MenuHeader>
-                        <MenuItem>Cut</MenuItem>
-                        <MenuItem>Copy</MenuItem>
-                        <MenuItem>Paste</MenuItem>
-                        <MenuDivider />
-                        <MenuItem>Print</MenuItem>
-                    </Menu>
-                </Example>
-
-                <Example data={codeExamples.combined} >
-                    <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
-                        <MenuItem>New File</MenuItem>
-                        <MenuItem>Save</MenuItem>
-                        <MenuDivider />
-                        <MenuHeader>Text settings</MenuHeader>
-
-                        <SubMenu label="Text color">
-                            <MenuRadioGroup
-                                value={textColor}
-                                onChange={e => setTextColor(e.value)}>
-                                <MenuItem value={'red'}>Red</MenuItem>
-                                <MenuItem value={'green'}>Green</MenuItem>
-                                <MenuItem value={'blue'}>Blue</MenuItem>
-                            </MenuRadioGroup>
-                        </SubMenu>
-
-                        <SubMenu label="Text style">
-                            <MenuItem type="checkbox" checked={isBold}
-                                onClick={e => setBold(e.checked)}>
-                                Bold
-                        </MenuItem>
-                            <MenuItem type="checkbox" checked={isItalic}
-                                onClick={e => setItalic(e.checked)}>
-                                Italic
-                        </MenuItem>
-                            <MenuItem type="checkbox" checked={isUnderline}
-                                onClick={e => setUnderline(e.checked)}>
-                                Underline
-                        </MenuItem>
-                        </SubMenu>
-                    </Menu>
-
-                    <div className="sample-text" style={{
-                        color: textColor,
-                        fontWeight: isBold ? 'bold' : 'initial',
-                        fontStyle: isItalic ? 'italic' : 'initial',
-                        textDecoration: isUnderline ? 'underline' : 'initial'
-                    }}>Sample text</div>
-                </Example>
-
+                <BasicMenuExample />
+                <SubmenuExample />
+                <RadioGroupExample />
+                <CheckBoxExample />
+                <HeaderAndDividerExample />
+                <CombinedExample />
+                <MenuItemLinkExample />
             </main >
 
-            <div className="place-holder" role="presentation"/>
+            <div className="place-holder" role="presentation" />
 
         </React.Fragment>
     );
 });
+
+function BasicMenuExample() {
+
+    return (
+        <Example initialFullSource={true} data={codeExamples.basicMenu} >
+            <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
+                <MenuItem>New File</MenuItem>
+                <MenuItem>Save</MenuItem>
+                <MenuItem>Close Window</MenuItem>
+            </Menu>
+        </Example>
+    );
+}
+
+function SubmenuExample() {
+
+    return (
+        <Example data={codeExamples.subMenu} >
+            <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
+                <MenuItem>New File</MenuItem>
+                <SubMenu label={'Open Recent'}>
+                    <MenuItem>index.html</MenuItem>
+                    <MenuItem>example.js</MenuItem>
+                    <MenuItem>about.css</MenuItem>
+                </SubMenu>
+                <MenuItem>Save</MenuItem>
+            </Menu>
+        </Example>
+    );
+}
+
+function RadioGroupExample() {
+    const [textColor, setTextColor] = useState('red');
+
+    return (
+        <Example data={codeExamples.radioGroup} >
+            <Menu menuButton={<MenuButton>Text color</MenuButton>}>
+                <MenuRadioGroup
+                    value={textColor}
+                    onChange={e => setTextColor(e.value)}>
+                    <MenuItem value={'red'}>Red</MenuItem>
+                    <MenuItem value={'green'}>Green</MenuItem>
+                    <MenuItem value={'blue'}>Blue</MenuItem>
+                </MenuRadioGroup>
+            </Menu>
+
+            <div className="sample-text" style={{ color: textColor }}>
+                Sample text
+            </div>
+        </Example>
+    );
+}
+
+function CheckBoxExample() {
+    const [isBold, setBold] = useState(true);
+    const [isItalic, setItalic] = useState(false);
+    const [isUnderline, setUnderline] = useState(false);
+
+    return (
+        <Example data={codeExamples.checkBox} >
+            <Menu menuButton={<MenuButton>Text style</MenuButton>}>
+                <MenuItem type="checkbox" checked={isBold}
+                    onClick={e => setBold(e.checked)}>
+                    Bold
+                </MenuItem>
+                <MenuItem type="checkbox" checked={isItalic}
+                    onClick={e => setItalic(e.checked)}>
+                    Italic
+                </MenuItem>
+                <MenuItem type="checkbox" checked={isUnderline}
+                    onClick={e => setUnderline(e.checked)}>
+                    Underline
+                </MenuItem>
+            </Menu>
+
+            <div className="sample-text" style={{
+                fontWeight: isBold ? 'bold' : 'initial',
+                fontStyle: isItalic ? 'italic' : 'initial',
+                textDecoration: isUnderline ? 'underline' : 'initial'
+            }}>Sample text</div>
+        </Example>
+    );
+}
+
+function HeaderAndDividerExample() {
+
+    return (
+        <Example data={codeExamples.headerAndDivider} >
+            <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
+                <MenuItem>New File</MenuItem>
+                <MenuItem>Save</MenuItem>
+                <MenuItem>Close Window</MenuItem>
+                <MenuDivider />
+                <MenuHeader>Edit</MenuHeader>
+                <MenuItem>Cut</MenuItem>
+                <MenuItem>Copy</MenuItem>
+                <MenuItem>Paste</MenuItem>
+                <MenuDivider />
+                <MenuItem>Print</MenuItem>
+            </Menu>
+        </Example>
+    );
+}
+
+function CombinedExample() {
+    const [textColor, setTextColor] = useState('red');
+    const [isBold, setBold] = useState(true);
+    const [isItalic, setItalic] = useState(false);
+    const [isUnderline, setUnderline] = useState(false);
+
+    return (
+        <Example data={codeExamples.combined} >
+            <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
+                <MenuItem>New File</MenuItem>
+                <MenuItem>Save</MenuItem>
+                <MenuDivider />
+                <MenuHeader>Text settings</MenuHeader>
+
+                <SubMenu label="Text color">
+                    <MenuRadioGroup
+                        value={textColor}
+                        onChange={e => setTextColor(e.value)}>
+                        <MenuItem value={'red'}>Red</MenuItem>
+                        <MenuItem value={'green'}>Green</MenuItem>
+                        <MenuItem value={'blue'}>Blue</MenuItem>
+                    </MenuRadioGroup>
+                </SubMenu>
+
+                <SubMenu label="Text style">
+                    <MenuItem type="checkbox" checked={isBold}
+                        onClick={e => setBold(e.checked)}>
+                        Bold
+                    </MenuItem>
+                    <MenuItem type="checkbox" checked={isItalic}
+                        onClick={e => setItalic(e.checked)}>
+                        Italic
+                    </MenuItem>
+                    <MenuItem type="checkbox" checked={isUnderline}
+                        onClick={e => setUnderline(e.checked)}>
+                        Underline
+                    </MenuItem>
+                </SubMenu>
+            </Menu>
+
+            <div className="sample-text" style={{
+                color: textColor,
+                fontWeight: isBold ? 'bold' : 'initial',
+                fontStyle: isItalic ? 'italic' : 'initial',
+                textDecoration: isUnderline ? 'underline' : 'initial'
+            }}>Sample text</div>
+        </Example>
+    );
+}
+
+function MenuItemLinkExample() {
+
+    return (
+        <Example data={codeExamples.combined} >
+            <Menu menuButton={<MenuButton>Open menu</MenuButton>}>
+                <MenuItem href="https://www.google.com/">Google</MenuItem>
+                <MenuItem href="https://github.com/szhsin/react-menu/"
+                    target="_blank">
+                    GitHub
+                </MenuItem>
+                <MenuItem>Normal item</MenuItem>
+            </Menu>
+        </Example>
+    );
+}
