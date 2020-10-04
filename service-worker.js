23c5a9dd8aab1569c7d20dcb09cd6dd1bd797e97
Zheng Song <41896553+szhsin@users.noreply.github.com>
2020-10-04 17:38:40 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c284a4adddad1738358bc048ece497c5.js"
+  "./precache-manifest.4410beb2f56a807aad0557e5c41c8318.js"
 );
 
 self.addEventListener('message', (event) => {
