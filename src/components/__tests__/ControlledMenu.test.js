8a161a481e82726230128ea526ac5f16d5d0feb9
Zheng Song <szhsin9@gmail.com>
2021-01-11 00:02:04 +1100

    Feature: keepOpen event prop for customising when menu is closed (#25)
    * Add keepOpen in events for customising when menu is closed
    
    * Add testing and document of keepOpen event property

--- src/components/__tests__/ControlledMenu.test.js
+++ src/components/__tests__/ControlledMenu.test.js
@@ -60,7 +60,7 @@ test('Test ControlledMenu with an anchor element', async () => {
     // Click on a menu item
     fireEvent.click(utils.queryMenuItem('Middle'));
     expect(onClick).toHaveBeenLastCalledWith({ value: 'Middle', checked: false });
-    expect(onClose).toHaveBeenLastCalledWith({ reason: 'click' });
+    expect(onClose).toHaveBeenLastCalledWith({ value: 'Middle', reason: 'click' });
 
     // Set isMounted to false, expect menu to be removed from DOM
     rerender(getMenu({ ...props, isMounted: false }));
