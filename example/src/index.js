e39e7db209ed4976bf29781b3a2cd81da7189095
Zheng Song <41896553+szhsin@users.noreply.github.com>
2021-05-01 13:49:20 +1000

    Update index.js

--- example/src/index.js
+++ example/src/index.js
@@ -1,9 +1,9 @@
 import '@szhsin/react-menu/dist/index.css';
 import '@szhsin/react-menu/dist/theme-dark.css';
-import './styles/index.scss';
+// import './styles/index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './testCases';
 
 ReactDOM.render(<App />, document.getElementById('root'));
