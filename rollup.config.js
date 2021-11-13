073ccdfa44621ae33cd0f59974b5f455c7b0b2a9
Zheng Song <41896553+szhsin@users.noreply.github.com>
2021-11-13 21:41:47 +1100

    Config updates for style-utils

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,22 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 
+const sharedConfig = {
+  external: ['react', 'react-dom', 'prop-types', 'react-transition-state'],
+  plugins: [nodeResolve(), babel({ babelHelpers: 'bundled' })],
+  treeshake: {
+    moduleSideEffects: false,
+    propertyReadSideEffects: false
+  }
+};
+
 /**
  * @type {import('rollup').RollupOptions}
  */
 export default [
   {
+    ...sharedConfig,
     input: 'src/index.js',
-    external: ['react', 'react-dom', 'prop-types', 'react-transition-state'],
-    plugins: [nodeResolve(), babel({ babelHelpers: 'bundled' })],
     output: [
       {
         file: 'dist/index.js',
@@ -20,10 +28,20 @@ export default [
         dir: 'dist/es',
         format: 'es'
       }
-    ],
-    treeshake: {
-      moduleSideEffects: false,
-      propertyReadSideEffects: false
-    }
+    ]
+  },
+  {
+    ...sharedConfig,
+    input: 'src/style-utils/index.js',
+    output: [
+      {
+        file: 'dist/style-utils/index.cjs.js',
+        format: 'cjs'
+      },
+      {
+        file: 'dist/style-utils/index.js',
+        format: 'es'
+      }
+    ]
   }
 ];
