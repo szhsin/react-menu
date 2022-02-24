a12c84b90cc54160198b0102590885569d73b9dc
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-02-25 00:20:19 +1100

    Remove `cloneChildren`

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
 export * from './constants';
 export * from './utils';
 export * from './propTypes';
-export { cloneChildren } from './cloneChildren';
 export { withHovering } from './withHovering';
