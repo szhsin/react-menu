2cebb1470c0601ce6878ddde60b7cfb9f207f465
Zheng Song <41896553+szhsin@users.noreply.github.com>
2022-03-10 00:22:54 +1100

    Fix FocusableItem `:active` styling

--- example/src/utils/index.js
+++ example/src/utils/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useLayoutEffect } from 'react';
 
 export const version = '3.0.0';
-export const build = '100';
+export const build = '101';
 export const DomInfoContext = React.createContext({});
 export const SettingContext = React.createContext({ theme: 'dark' });
 export const TocContext = React.createContext({}); // Table of contents
