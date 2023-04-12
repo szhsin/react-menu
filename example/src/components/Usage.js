98b42f89b469c8706684c88364b7911f2ada4a72
Mike Dijkstra <michael@dijkstra.com.au>
2023-04-12 12:56:22 +1000

    fix: Typo in Usage example

--- example/src/components/Usage.js
+++ example/src/components/Usage.js
@@ -763,8 +763,8 @@ const HoverMenu = () => {
 
 const HoverMenuWithTransition = () => {
   const ref = useRef(null);
-  const [menuState, toogle] = useMenuState({ transition: true });
-  const { anchorProps, hoverProps } = useHover(menuState.state, toogle);
+  const [menuState, toggle] = useMenuState({ transition: true });
+  const { anchorProps, hoverProps } = useHover(menuState.state, toggle);
 
   return (
     <>
@@ -777,7 +777,7 @@ const HoverMenuWithTransition = () => {
         arrow
         align="end"
         anchorRef={ref}
-        onClose={() => toogle(false)}
+        onClose={() => toggle(false)}
       >
         <MenuItem>Cut</MenuItem>
         <MenuItem>Copy</MenuItem>
