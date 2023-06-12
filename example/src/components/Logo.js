9410989f20afd53d0d6163a547df818af44cdfda
Zheng Song <szhsin9@gmail.com>
2023-06-12 16:57:45 +1000

    Version menu for v4 (#971)

--- example/src/components/Logo.js
+++ example/src/components/Logo.js
@@ -21,15 +21,19 @@ export const Logo = React.memo(function Logo() {
       className="version-menu"
     >
       <MenuHeader>Version history</MenuHeader>
+      <MenuItem href="https://szhsin.github.io/react-menu-v3">v3.5.x</MenuItem>
       <MenuItem href="https://szhsin.github.io/react-menu-v2">v2.3.x</MenuItem>
       <MenuItem href="https://szhsin.github.io/react-menu-v1">v1.11.x</MenuItem>
       <MenuDivider />
       <MenuHeader>Migration guide</MenuHeader>
-      <MenuItem href="https://github.com/szhsin/react-menu/wiki/Migration-from-v2-to-v3">
-        v2 to v3
+      <MenuItem href="https://github.com/szhsin/react-menu/blob/master/docs/migration/v4.md">
+        migrating to v4
       </MenuItem>
-      <MenuItem href="https://github.com/szhsin/react-menu/wiki/Migration-from-v1-to-v2">
-        v1 to v2
+      <MenuItem href="https://github.com/szhsin/react-menu/blob/master/docs/migration/v3.md">
+        migrating to v3
+      </MenuItem>
+      <MenuItem href="https://github.com/szhsin/react-menu/blob/master/docs/migration/v2.md">
+        migrating to v2
       </MenuItem>
     </Menu>
   );
